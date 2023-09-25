9a4dca0276253ebe285cfa790a6fa585d755edb1
Kseniia <ksenia.guseva789@gmail.com>
2023-09-25 11:25:18 +0300

    update getResponse func

--- src/getResponse.js
+++ src/getResponse.js
@@ -1,13 +1,18 @@
 import axios from 'axios';
 
+// const getResponse = (url) => {
+//   console.log('getting response');
+//   const promise = axios.get(`https://allorigins.hexlet.app/get?disableCache=true&url=${url}`)
+//     .then((result) => result)
+//     .catch(() => {
+//       throw new Error('errorNetwork');
+//     });
+//   return promise;
+// };
+
 const getResponse = (url) => {
   console.log('getting response');
-  const promise = axios.get(`https://allorigins.hexlet.app/get?disableCache=true&url=${url}`)
-    .then((result) => result)
-    .catch(() => {
-      throw new Error('errorNetwork');
-    });
-  return promise;
+  return axios.get(`https://allorigins.hexlet.app/get?disableCache=true&url=${url}`);
 };
 
 export default getResponse;
